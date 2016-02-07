eaaf4daab751296ed572d9405a5e68fc7952b3fe
hypo9eal <uon@uonsMBA.local>
2016-02-07 22:36:48 +0900

    change order

--- update.sh
+++ update.sh
@@ -10,11 +10,11 @@ brew cask update
 echo ">> brew cask doctor..."
 brew cask doctor
 
-echo ">> apm update..."
-apm update
-
 echo ">> npm update -g..."
 npm update -g
 
 echo ">> gem update..."
 gem update
+
+echo ">> apm update..."
+apm update
