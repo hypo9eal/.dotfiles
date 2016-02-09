20d6efa281fc99b626a5de015aee52c3f2e37f87
hypo9eal <uon@uonsMBA.local>
2016-02-09 10:11:05 +0900

    change order

--- setup.sh
+++ setup.sh
@@ -5,18 +5,6 @@ DOTFILES_ROOT=~/.dotfiles
 echo ">> cloning .dotfiles..."
 git clone git@github.com:hypo9eal/.dotfiles.git ${DOTFILES_ROOT} --recursive
 
-### IFTTT dash
-echo ">> installing dash..."
-bash <(curl -fsSL https://raw.githubusercontent.com/IFTTT/dash/master/bin/bootstrap)
-
-### anyenv
-echo ">> installing anyenv..."
-git clone https://github.com/riywo/anyenv ~/.anyenv
-
-### ndenv
-echo ">> installing ndenv..."
-anyenv install ndenv
-
 ### zprezto
 echo ">> installing zprezto..."
 zsh
@@ -37,12 +25,25 @@ echo ">> activate zsh..."
 chsh -s /bin/zsh
 source ~/.zprofile
 
-### symbolic link
-echo ">> symbolic link ~/.atom -> ${DOTFILES_ROOT}/.atom"
-ln -s ${DOTFILES_ROOT}/.atom ~/.atom
+### IFTTT dash
+echo ">> installing dash..."
+bash <(curl -fsSL https://raw.githubusercontent.com/IFTTT/dash/master/bin/bootstrap)
 
+### anyenv
+echo ">> installing anyenv..."
+git clone https://github.com/riywo/anyenv ~/.anyenv
+
+### ndenv
+echo ">> installing ndenv..."
+anyenv install ndenv
+
+### symbolic link
 echo ">> symbolic link ~/.vimrc -> ${DOTFILES_ROOT}/.vimrc"
 ln -s ${DOTFILES_ROOT}/.vimrc ~/.vimrc
 
 echo ">> symbolic link ~/.gitconfig -> ${DOTFILES_ROOT}/.gitconfig"
 ln -s ${DOTFILES_ROOT}/.gitconfig ~/.gitconfig
+
+echo ">> symbolic link ~/.atom -> ${DOTFILES_ROOT}/.atom"
+ln -s ${DOTFILES_ROOT}/.atom ~/.atom
+
